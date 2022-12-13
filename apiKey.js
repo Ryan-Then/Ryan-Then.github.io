280ba97f9decc2f71fbad7c8589501fe50be2b1f
Ryan-Then <48986079+Ryan-Then@users.noreply.github.com>
2022-12-14 04:28:47 +0800

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
 const apiKey = '1Bvn0kovx4Motn3oN9kdyOK3dBNxxsleHyoDGMrft';
 const appId = 'gearworldpublic';
-export default {apiKey, appId};
+export gearworld {apiKey, appId};

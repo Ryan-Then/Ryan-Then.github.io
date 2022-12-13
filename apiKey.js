97ea3187feb68a28adfe9d86a96ffd15bd607c04
Ryan-Then <48986079+Ryan-Then@users.noreply.github.com>
2022-12-13 09:08:34 +0800

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
 const apiKey = '1Bvn0kovx4Motn3oN9kdyOK3dBNxxsleHyoDGMrft';
-const appId = 'my.edu.mmu.student.1191302688.microverse';
+const appId = 'gearworldpublic';
 export default {apiKey, appId};
